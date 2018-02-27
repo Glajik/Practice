35d6f0652b86049159f04cdc45a7c7487e59ca96
Viacheslav <glajik@gmail.com>
2018-02-27 13:45:53 +0200

    Make some refactoring and add two lines into test. test passed

--- __tests__/withoutTwoZeros.test.js
+++ __tests__/withoutTwoZeros.test.js
@@ -2,13 +2,13 @@ import solution from '../src/withoutTwoZeros';
 
 describe('Data', () => {
 	it('#withoutTwoZeros', () => {
-
 		// usage: solution(zeros, ones);
 		// return amount of combination without two zeros nearby
 		expect(solution(1, 1)).toBe(2);
 		expect(solution(2, 2)).toBe(3);
 		expect(solution(3, 1)).toBe(0);
 		expect(solution(2, 1)).toBe(1);
-
+		expect(solution(1, 3)).toBe(4);
+		expect(solution(2, 4)).toBe(10);
 	});
 });
--- src/withoutTwoZeros.js
+++ src/withoutTwoZeros.js
@@ -3,60 +3,27 @@
 // Factorial
 const F = n => (n <= 1) ? 1 : n * F(n-1);
 
-// Sum
-const S = n => (n <= 1) ? n + 1 : n + 1 + S(n-1);
-
-// for debug
-const view = (zeros, ones) => {
-  return '0'.repeat(zeros).concat('1'.repeat(ones));
-};
-
-const withoutTwoZeros = (zeros, ones) => {
-  // [0011]
-  console.log(`Node: ${view(zeros, ones)}`);
-
-  if (zeros === 1 && ones === 1) return 2; // [01] [10]
-  if (ones === 0) return -1; // [00] or [000000000]
-  if (zeros === 0) return 1; // [11] or [111111111]
-
-  // 0[011] 
-  console.log('/');
-  const subtree1 = withoutTwoZeros(zeros - 1, ones);
-
-  // [001]1
-  console.log('\\');
-  const subtree2 = withoutTwoZeros(zeros, ones - 1);
-
-  console.log(`left: ${subtree1} right: ${subtree2}`);
-
-  if (subtree1 < 0 || subtree2 < 0) {
-    console.log('Parent Zero');
-    if (subtree1 < 0 && subtree2 < 0) return subtree1 + subtree2 - 1;
-    else if (subtree1 < 0) return subtree1 - 1;
-    else if (subtree2 < 0) return subtree2 - 1;
-  }
-  const result = subtree1 + subtree2;
-
-  console.log(`result: ${result}`);
-
+const countTwoZeros = (zeros, ones) => {
+  if (zeros === 1 && ones === 1) return 0; // [01] [10]
+  if (ones === 0) return 1; // [00] or [000000000]
+  if (zeros === 0) return 0; // [11] or [111111111]
+  const subtree1 = countTwoZeros(zeros - 1, ones);
+  const subtree2 = countTwoZeros(zeros, ones - 1);
+  if (subtree1 > 0 || subtree2 > 0) return subtree1 + subtree2 + 1;
   return 0;
 }
 
 const solution = (zeros, ones) => {
-  console.log(`--- Input: ${view(zeros, ones)} -----------------------`);
-  const n = zeros + ones; // Количество элементов в множестве
-  const k = zeros; // Количество элементов в подмножестве (выборка)
-  // Число сочетаний
-  const result = F(n) / ( F(k) * F(n - k) );
-  console.log (`Число сочетаний ${k} от ${n} = ${result}`);
+  console.log(`--- Inpu - zeros:${zeros} ones:${ones} -----------------------`);
 
   // Количество перестановок с повторением
-  const result1 = F(n) / ( F(zeros) * F(ones) );
-  console.log (`Число перестановок с повторением 0:${zeros} и 1:${ones} = ${result1}`);
+  const count = F(zeros + ones) / ( F(zeros) * F(ones) );
+  console.log (`Число перестановок = ${count}`);
+
   // Количество кобинаций с двумя нулями
-  const result2 = withoutTwoZeros(zeros, ones);
-  console.log(`Число перестановок с двумя нулями ${result2}`);
-  return result2 < 0 ? result1 + result2 : result1;
+  const result = count - countTwoZeros(zeros, ones);
+  console.log(`Число перестановок без двух нулей  ${result}`);
+  return result;
 };
 
 export default solution;
